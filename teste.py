8b7bde7c95c2175bd22b8f1cd254c4bde5a62f7e
Weslley <weslleynfs@gmail.com>
2019-04-16 21:51:33 -0400

    Commit de algumas alterações e idéias para implementar o script de exportação de dados do .csv para os models

--- teste.py
+++ teste.py
@@ -1,11 +1,25 @@
-import csv
+'''import csv, io
 with open('levantamento_2.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
-    cabecalho = True
+    #cabecalho = True
+    #next(csv_reader)
+    distrito = {}
+    distrito['descricao_distrito'] = []
     for row in csv_reader:
-        '''if cabecalho:
-            print(f'Nomes das colunas: {", ".join(row)}')
-            cabecalho = False
-        else:
-            print(f'{", ".join(row)}')'''
-        print(row)
+        distrito['descricao_distrito'].append(row[4])
+
+print(distrito['descricao_distrito'])'''
+dicionario = {}
+array_dicionarios = []
+
+dicionario['nome'] = 'Weslley'
+dicionario['idade'] = 22
+
+array_dicionarios.append(dicionario)
+
+dicionario['nome'] = 'Beatriz'
+dicionario['idade'] = 21
+
+array_dicionarios.append(dicionario)
+
+print(array_dicionarios)
